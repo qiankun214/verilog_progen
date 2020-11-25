7169ab73c643794aa00a0e05abd188b2e1ce71d4
qiankun <qiankun96214@outlook.com>
2020-11-25 17:53:27 +0800

    add

--- script/module_info.py
+++ script/module_info.py
@@ -30,14 +30,91 @@ class module_info(object):
                 self.output_port.append(port_name)
 
     def moduledef_gen(self):
-        pass
+        data = ["module {}".format(self.name),]
+        if len(self.parameter) == 0:
+            data.append("(")
+        else:
+            data.append("#(")
+            param_list = ["\tparameter {} = {}".format(param,self.parameter[param][0]) for param in self.parameter]
+            data.append(",\n".join(param_list))
+            data.append(") (")
+        port_list = []
+        for p in self.port:
+            port_info = self.port[p]
+            if port_info[1] == "1":
+                port_list.append("\t{} {}".format(port_info[0],p))
+            else:
+                port_list.append("\t{} [{} - 1 : 0] {}".format(port_info[0],port_info[1],p))
+        data.append(",\n".join(port_list))
+        data.append(");")
+        return "\n".join(data)
 
-    def instance_gen(self):
-        pass
+    def instance_gen(self, inst_name, parent_param={}):
+        inst = ["\n//instance {} module {}".format(inst_name,self.name)]
+        # parameter generate
+        for key in self.parameter:
+            param_info = self.parameter[key]
+            if parent_param.get(key) is not None:
+                inst.append('parameter {}_{} = {};'.format(inst_name,key,key))
+            else:
+                inst.append('parameter {}_{} = {}; // cannot find,use default'.format(inst_name,key,param_info[0]))
+                print("WARING:cannot find {}'parent paramter of {},use default".format(key,inst_name))
+        # port generate
+        for p in self.port:
+            p_type,p_width,_ = self.port[p]
+            tmp = "wire"
+            if p_width.strip() != "1":
+                if "`" not in p_width:
+                    tmp += " [{}_{} - 1:0]".format(inst_name,p_width)
+                else:
+                    tmp += " [{} - 1:0]".format(p_width)
+            tmp += " {}_{};".format(inst_name,p)
+            inst.append(tmp)
+
+        if len(self.parameter) == 0:
+            inst.append("{} {} (".format(self.name,inst_name))
+        else:
+            inst.append("{} #(".format(self.name))
+            param = ["\t.{}({}_{})".format(key,inst_name,key) for key in self.parameter]
+            param = ",\n".join(param)
+            inst.append(param)
+            inst.append(") {} (".format(inst_name))
+
+        port_text = ["\t.{}({}_{})".format(key,inst_name,key) for key in self.port]
+        port_text = ",\n".join(port_text)
+        inst.append(port_text)
+        inst.append(");")
+        return "\n".join(inst)
 
     def testbench_gen(self):
         pass
 
-    def design_gen(self):
+    def design_gen(self,link_text=""):
         pass
 
+class linker(object):
+
+    def __init__(self):
+        super(linker,self).__init__()
+        self.input_port_pool = {}
+        self.output_port_pool = {}
+
+    def input_append(self,name,port_list):
+        pass
+    # {'port name':['inst1','inst2']}
+    def output_append(self,name,port_list):
+        pass
+
+    def generate_assign(self,request):
+        # request:
+            # [
+            #     "inst_test_b.din_valid",
+            #     "inst_test_a.dout_valid"
+            # ],
+
+if __name__=='__main__':
+    test = module_info("./example/info/test.json")
+    # print(test.moduledef_gen())
+    print(test.instance_gen("tse"))
+    print(test.input_port)
+    print(test.output_port)
